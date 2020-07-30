d70a5670df9886e28af4c315f6b91cf22666f9b5
yuuukiii09101 <68684617+yuuukiii09101@users.noreply.github.com>
2020-07-30 23:56:47 +0900

    Add files via upload

---
+++ js/unilife.js
@@ -0,0 +1,23 @@
+$('#faculty').click(function(){
+    
+    
+   $('#contentsoff').toggle();
+});
+
+$('#sugimotoseminar').click(function(){
+    
+    
+   $('#contentsofs').toggle();
+});
+
+$('#introhobby').click(function(){
+    
+    
+   $('#contentsofi').toggle();
+});
+
+$('#clubactivity').click(function(){
+    
+    
+   $('#contentsofc').toggle();
+});
